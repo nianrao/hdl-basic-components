88da65ec388534d667e6840a6548b9af0889a866
Niankun Rao <conan.rnk@gmail.com>
2022-05-01 17:10:48 -0700

    [fifo-fix] add conversion of gray code ptr to binary ptr at the destination clock

--- rtl/fifo/src/fifo_2clk.sv
+++ rtl/fifo/src/fifo_2clk.sv
@@ -30,11 +30,13 @@ module fifo_2clk #(
 
   // write interface
   logic [ADDR_WIDTH:0] wr_ptr, wr_ptr_gray;
-  logic [ADDR_WIDTH:0] rd_ptr_sync_wr_clk[SYNC_STAGE-1:0];
+  logic [ADDR_WIDTH:0] rd_ptr_gray_sync_wr_clk[SYNC_STAGE-1:0];
+  logic [ADDR_WIDTH:0] rd_ptr_binary_sync_wr_clk;
 
   // read interface
   logic [ADDR_WIDTH:0] rd_ptr, rd_ptr_gray;
-  logic [ADDR_WIDTH:0] wr_ptr_sync_rd_clk[SYNC_STAGE-1:0];
+  logic [ADDR_WIDTH:0] wr_ptr_gray_sync_rd_clk[SYNC_STAGE-1:0];
+  logic [ADDR_WIDTH:0] wr_ptr_binary_sync_rd_clk;
   logic [WIDTH-1:0] dout_temp;
 
   //////////////////////////////////////////////////////////////////////////////
@@ -90,29 +92,42 @@ module fifo_2clk #(
   //////////////////////////////////////////////////////////////////////////////
   always_ff @(posedge wr_clk or posedge rst_async) begin : rd_ptr_sync
     if (rst_async) begin
-      rd_ptr_sync_wr_clk <= '{default: '0};
+      rd_ptr_gray_sync_wr_clk <= '{default: '0};
     end else begin
-      rd_ptr_sync_wr_clk <= {
-        rd_ptr_sync_wr_clk[0+:(SYNC_STAGE-1)], rd_ptr_gray
+      rd_ptr_gray_sync_wr_clk <= {
+        rd_ptr_gray_sync_wr_clk[0+:(SYNC_STAGE-1)], rd_ptr_gray
       };
     end
   end
 
-  assign full = (wr_ptr[0 +: ADDR_WIDTH] == rd_ptr_sync_wr_clk[SYNC_STAGE-1][0 +: ADDR_WIDTH]) &&
-                (wr_ptr[ADDR_WIDTH] != rd_ptr_sync_wr_clk[SYNC_STAGE-1][ADDR_WIDTH]);
+  gray_to_binary #(
+      .WIDTH(ADDR_WIDTH)
+  ) rd_ptr_gray_to_binary_inst (
+      .gray  (rd_ptr_gray_sync_wr_clk[SYNC_STAGE-1]),
+      .binary(rd_ptr_binary_sync_wr_clk)
+  );
 
+  assign full = (wr_ptr[0 +: ADDR_WIDTH] == rd_ptr_binary_sync_wr_clk[0 +: ADDR_WIDTH]) &&
+                (wr_ptr[ADDR_WIDTH] != rd_ptr_binary_sync_wr_clk[ADDR_WIDTH]);
 
   always_ff @(posedge rd_clk or posedge rst_async) begin : wr_ptr_sync
     if (rst_async) begin
-      wr_ptr_sync_rd_clk <= '{default: '0};
+      wr_ptr_gray_sync_rd_clk <= '{default: '0};
     end else begin
-      wr_ptr_sync_rd_clk <= {
-        wr_ptr_sync_rd_clk[0+:(SYNC_STAGE-1)], wr_ptr_gray
+      wr_ptr_gray_sync_rd_clk <= {
+        wr_ptr_gray_sync_rd_clk[0+:(SYNC_STAGE-1)], wr_ptr_gray
       };
     end
   end
 
-  assign empty = (rd_ptr == wr_ptr_sync_rd_clk[SYNC_STAGE-1]);
+  gray_to_binary #(
+      .WIDTH(ADDR_WIDTH)
+  ) wr_ptr_gray_to_binary_inst (
+      .gray  (wr_ptr_gray_sync_rd_clk[SYNC_STAGE-1]),
+      .binary(wr_ptr_binary_sync_rd_clk)
+  );
+
+  assign empty = (rd_ptr == wr_ptr_binary_sync_rd_clk);
 
 endmodule
 
